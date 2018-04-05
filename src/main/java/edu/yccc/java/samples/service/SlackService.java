002457d8846aedd26bca218049874b41d7655069
MICHAEL BOURGEOIS <Michael.Bourgeois@LibertyMutual.com>
2018-04-05 09:20:49 -0400

    Init commit

---
+++ src/main/java/edu/yccc/java/samples/service/SlackService.java
@@ -0,0 +1,28 @@
+package edu.yccc.java.samples.service;
+
+import org.springframework.stereotype.Component;
+
+import net.gpedro.integrations.slack.SlackApi;
+import net.gpedro.integrations.slack.SlackMessage;
+
+/**
+ * First pass integration with slack. 
+ * 
+ * 
+ * @author Mike Bourgeois
+ *
+ */
+
+@Component
+public class SlackService 
+{
+	// #integration webhook url.  Create your own channel and add the webhook app.  Then update this with your value.
+	private String webHookUrl = "https://hooks.slack.com/services/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";
+	private SlackApi api = new SlackApi(webHookUrl);
+	
+	public void sendMessage(String channel, String userName, String message) 
+	{
+		api.call(new SlackMessage(channel, userName, message));
+	}
+
+}
